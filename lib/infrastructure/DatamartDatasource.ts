30969245acb720b552ba6e4f4f262f4f9696e56a
lucas.herrador <lucas.herrador@octo.com>
2023-07-21 11:54:49 +0200

    feat: create request datamart builder

--- lib/infrastructure/DatamartDatasource.ts
+++ lib/infrastructure/DatamartDatasource.ts
@@ -2,15 +2,17 @@ import { knexDatamart } from '../common/db/knex-database-connections.js';
 import { logger } from '../common/logger/logger.js';
 import { DatamartQuery } from '../domain/models/DatamartQuery.ts';
 import { DatamartResponse } from '../domain/models/DatamartResponse.ts';
+import { RequestBuilder } from './builder/RequestBuilder.ts';
 
 export interface DatamartRepository {
   find(_datamartQuery: DatamartQuery): Promise<DatamartResponse>;
 }
 class DatamartRepositoryImpl implements DatamartRepository {
   async find(datamartQuery: DatamartQuery): Promise<DatamartResponse> {
+    const requestBuilder = new RequestBuilder(datamartQuery);
     try {
       // eslint-disable-next-line knex/avoid-injections
-      const result = await knexDatamart.raw(datamartQuery.query);
+      const result = await knexDatamart.raw(requestBuilder.build());
       return result['rows'];
     } catch (e) {
       logger.error(`Error while executing query: ${datamartQuery.query}`, e);
