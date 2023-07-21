33e6848cf0b232ca1cb9df6fa48a0da2148bf768
Aurelie Crouillebois <aurelie.crouillebois@gmail.com>
2023-07-21 15:27:32 +0200

    feat: add date_time param type and remove date_array
    Co-authored-by: Thomas Estrabaud <thomas.estrabaud@pix.fr>
    Co-authored-by: Lucas HERRADOR <lucas.herrador@pix.fr>
    Co-authored-by: Gilles Vieira <gilles.vieira@pix.fr>

--- lib/domain/RequestCatalogItem.ts
+++ lib/domain/RequestCatalogItem.ts
@@ -13,10 +13,10 @@ export enum ParamType {
   STRING = 'string',
   INT = 'int',
   DATE = 'date',
+  DATE_TIME = 'date-time',
   FLOAT = 'float',
   BOOLEAN = 'boolean',
   STRING_ARRAY = 'string-array',
   INT_ARRAY = 'int-array',
-  DATE_ARRAY = 'date-array',
   FLOAT_ARRAY = 'float-array',
 }
