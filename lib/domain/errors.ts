f1465f8458cff0b4a13738eff01b583fcae3430f
Gilles Vieira <vieira.gille@gmail.com>
2024-08-21 14:46:54 +0200

    feat: add usecase to check if user is blocked
    Signed-off-by: Gilles Vieira <vieira.gille@gmail.com>

--- lib/domain/errors.ts
+++ lib/domain/errors.ts
@@ -1,5 +1,21 @@
-export class NotFoundError extends Error {
+class DomainError extends Error {
+  code: string;
+  meta: string;
+  constructor(message, code?, meta?) {
+    super(message);
+    this.code = code;
+    this.meta = meta;
+  }
+}
+
+export class NotFoundError extends DomainError {
   constructor(message) {
     super(message);
+  }
+}
+
+export class UserIsTemporaryBlocked extends DomainError {
+  constructor(message = 'User has been temporary blocked.', code = 'USER_HAS_BEEN_TEMPORARY_BLOCKED') {
+    super(message, code);
   }
 }
