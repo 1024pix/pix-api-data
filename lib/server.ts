29d5cb1a97fe342755615d1bc7528c63b09512ba
Vincent Hardouin <vincent.hardouin@pix.fr>
2024-09-18 11:15:19 +0200

    chore: add oppsy

--- lib/server.ts
+++ lib/server.ts
@@ -2,10 +2,13 @@ import { env } from 'node:process';
 import type { Server, ServerOptions } from '@hapi/hapi';
 import Hapi from '@hapi/hapi';
 
+import Oppsy from '@1024pix/oppsy';
+import { knexAPI, knexDatamart } from './common/db/knex-database-connections.js';
 import { authentication } from './infrastructure/authentication.js';
 import { plugins } from './infrastructure/plugins/plugins.js';
 import { routes } from './routes.js';
 import { handleDomainAndHttpErrors } from './application/pre-response-utils.js';
+import { config } from './common/config.js';
 
 const createBareServer = function (): Server {
   const serverConfiguration: ServerOptions = {
@@ -51,11 +54,41 @@ const setupErrorHandling = function (server: Server) {
   server.ext('onPreResponse', handleDomainAndHttpErrors);
 };
 
+const enableOpsMetrics = function (server) {
+  const oppsy = new Oppsy(server);
+
+  oppsy.on('ops', (data) => {
+    const apiKnexPool = knexAPI.client.pool;
+    const datamartKnexPool = knexDatamart.client.pool;
+    server.log(['ops'], {
+      ...data,
+      knexPool: {
+        apiData: {
+          used: apiKnexPool.numUsed(),
+          free: apiKnexPool.numFree(),
+          pendingAcquires: apiKnexPool.numPendingAcquires(),
+          pendingCreates: apiKnexPool.numPendingCreates(),
+        },
+        datamart: {
+          used: datamartKnexPool.numUsed(),
+          free: datamartKnexPool.numFree(),
+          pendingAcquires: datamartKnexPool.numPendingAcquires(),
+          pendingCreates: datamartKnexPool.numPendingCreates(),
+        },
+      },
+    });
+  });
+
+  oppsy.start(config.logging.opsEventIntervalInSeconds * 1000);
+  server.oppsy = oppsy;
+};
+
 async function createServer(): Promise<Server> {
   const server = createBareServer();
   setupErrorHandling(server);
   setupAuthentication(server);
   await setupRoutesAndPlugins(server);
+  enableOpsMetrics(server);
   return server;
 }
 
